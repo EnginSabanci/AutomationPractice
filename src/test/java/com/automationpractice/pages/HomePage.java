bd7dbb03318d168fcf47afd80e0f494cd3d2ab0c
Ozen Turkoz <oturkoz@gmail.com>
2020-11-11 16:47:19 -0800

    Commented out unused import and updated misc.xml with JDK 11

--- src/test/java/com/automationpractice/pages/HomePage.java
+++ src/test/java/com/automationpractice/pages/HomePage.java
@@ -1,10 +1,10 @@
 package com.automationpractice.pages;
-import org.openqa.selenium.*;
+//import org.openqa.selenium.*;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.Test;
+//import org.testng.annotations.Test;
 import utils.Driver;
 
 public class HomePage {
