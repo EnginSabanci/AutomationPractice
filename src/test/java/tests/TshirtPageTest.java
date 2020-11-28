30d77a14c96ad49508533650b73841de1f344977
Yasar ERKAN <ysr.erk2019@gmail.com>
2020-11-27 19:35:34 -0500

    update  for comment

--- src/test/java/tests/TshirtPageTest.java
+++ src/test/java/tests/TshirtPageTest.java
@@ -1,14 +1,24 @@
 package tests;
 
+import com.automationpractice.pages.HomePage;
 import com.automationpractice.pages.TshirtPage;
 import org.testng.annotations.Test;
+import utils.BrowserUtils;
 
 public class TshirtPageTest extends TestBase{
-    @Test(dependsOnMethods="HomePageTest.test1")
+    @Test
     public void tshirtlabelisdisplayed() throws InterruptedException {
+        //objects
+        HomePage homePage=new HomePage();
+        BrowserUtils browserUtils=new BrowserUtils();
         TshirtPage tshirtPage=new TshirtPage();
 
+        homePage.clickTshirt();
+        browserUtils.wait(1000);
         tshirtPage.tshirtlabelcontrol();
+        tshirtPage.clickAddChart();
+
+
 
 
 
